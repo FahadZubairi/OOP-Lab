41d8c411f12258770d4b214ee3355a303e7e504e
Fahad Zubairi <154765524+FahadZubairi@users.noreply.github.com>
2025-04-23 13:26:05 +0500

    Update Task1.cpp

--- Lab9/Task1.cpp
+++ Lab9/Task1.cpp
@@ -1,1 +1,18 @@
-#include<iostream>
+#include <iostream>
+using namespace std;
+
+class Wallet
+{
+    double balance;
+
+public:
+    Wallet(double b)
+    {
+        if (b >= 0)
+            balance = b;
+        else
+            cerr << "Balance cannot be negative\n";
+    }
+    
+};
+int main() {}
